4882c33b875b97ed82dd361a0bc3771600779c86
ironraymond04 <raymondgalanza.202301013@gmail.com>
2024-12-12 06:30:50 +0000

    updated 12-12-24

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_5hflxl5", "template_h2opg5w",parms).then(alert("Email sent"))
+}
